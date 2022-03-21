ca1baa39b900331be71bc4ffa6fac82e61009142
bv@bartosarchitecture.com <bv@bartosarchitecture.com>
2022-03-21 13:09:36 -0700

    Accounting for what items was selected and opening modal.

--- src/components/MovieTable.js
+++ src/components/MovieTable.js
@@ -7,46 +7,66 @@ import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
 import Button from "@mui/material/Button";
+import MovieModal from "./MovieModal";
 
 export default function MovieTable(props) {
   const movieData = props.movieData;
   const rows = movieData;
+  const [openModal, setOpenModal] = React.useState(false);
+  const [id, setId] = React.useState(null);
+  const [actionType, setActionType] = React.useState(null);
+
+  function handleOpenModal(id, actionType){
+    setOpenModal(true);
+    setId(id);
+    setActionType(actionType)
+  }
 
   return (
-    <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 650 }} aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            <TableCell>id</TableCell>
-            <TableCell align="left">Title</TableCell>
-            <TableCell align="left">Ratig</TableCell>
-            <TableCell align="center" colSpan={3}>Actions</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {rows.map((row) => (
-            <TableRow
-              key={row.id}
-              sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-            >
-              <TableCell component="th" scope="row">
-                {row.id}
-              </TableCell>
-              <TableCell align="left">{row.name}</TableCell>
-              <TableCell align="left">{row.rating}</TableCell>
-              <TableCell align="left">
-                <Button variant="contained">Show</Button>
-              </TableCell>
-              <TableCell align="left">
-                <Button variant="contained" color="success" >Edit</Button>
-              </TableCell>
-              <TableCell align="left">
-                <Button variant="contained" color="error" >Delete</Button>
+    <>
+      <MovieModal open={openModal} setOpen={setOpenModal} id={id} actionType={actionType} />
+
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell>id</TableCell>
+              <TableCell align="left">Title</TableCell>
+              <TableCell align="left">Ratig</TableCell>
+              <TableCell align="center" colSpan={3}>
+                Actions
               </TableCell>
             </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+          </TableHead>
+          <TableBody>
+            {rows.map((row) => (
+              <TableRow
+                key={row.id}
+                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+              >
+                <TableCell component="th" scope="row">
+                  {row.id}
+                </TableCell>
+                <TableCell align="left">{row.name}</TableCell>
+                <TableCell align="left">{row.rating}</TableCell>
+                <TableCell align="left">
+                  <Button variant="contained" id={row.id} onClick={ (e)=>(handleOpenModal(e.target.id, 'show')) } >Show</Button>
+                </TableCell>
+                <TableCell align="left">
+                  <Button variant="contained" color="success" id={row.id} onClick={ (e)=>(handleOpenModal(e.target.id, 'edit')) } >
+                    Edit
+                  </Button>
+                </TableCell>
+                <TableCell align="left">
+                  <Button variant="contained" color="error" id={row.id} onClick={ (e)=>(handleOpenModal(e.target.id, 'delete')) }>
+                    Delete
+                  </Button>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </>
   );
 }
