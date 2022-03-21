1d109d47e0ae79c9ac50e8871131df12b5b457e6
bv@bartosarchitecture.com <bv@bartosarchitecture.com>
2022-03-21 12:08:02 -0700

    Fetch and grid structure.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import Grid from "@mui/material/Grid";
+import { useEffect, useState } from "react";
+import MovieTable from './components/MovieTable';
 
 function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3001/movies")
+      .then((res) => res.json())
+      .then(
+        (result) => {
+          console.log("Success:");
+          console.log(result);
+          setData(result);
+        },
+        (error) => {
+          console.log("Error:");
+          console.log(error);
+        }
+      );
+  }, []);
+
+  const columnsTitles = [
+    { field: "id", headerName: "ID", width: 9 },
+    { field: "name", headerName: "Name", editable: false, width: 300  },
+    { field: "rating", headerName: "Rating", editable: false },
+  ];
+
+  const rows = data;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Grid
+      container
+      spacing={4}
+      direction="column"
+      alignItems="center"
+      justifyContent="center"  
+    >
+      <Grid item xs={3}>
+        <div className="App">
+          <h1>Movies List</h1>
+        </div>
+      </Grid>
+
+      <Grid item>
+          <MovieTable movieData={data} /> 
+      </Grid>
+    </Grid>
   );
 }
 
